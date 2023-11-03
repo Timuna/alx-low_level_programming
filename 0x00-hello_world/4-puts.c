97e8c6a6697310356482b293c34243a8d9dd5e13
Timuna <chriskambimbi@gmail.com>
2023-11-03 15:35:12 +0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,4 +3,15 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+return (0)jj#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+;
